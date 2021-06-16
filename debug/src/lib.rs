25e9294441e51add15415b52d329ba230599d0e6
wanghui <wh_linux@126.com>
2021-06-16 11:24:48 +0800

    debug 2/8

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,32 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{self, parse, DeriveInput};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast: DeriveInput = parse(input).unwrap();
+    let name = &ast.ident;
 
-    unimplemented!()
+    let mut debug_fields = vec![];
+
+    if let syn::Data::Struct(ds) = ast.data {
+        if let syn::Fields::Named(fields) = ds.fields {
+            for field in fields.named.iter() {
+                let field_name = field.ident.clone().unwrap();
+                debug_fields.push(quote!{
+                    field(stringify!(#field_name), &self.#field_name)
+                });
+            }
+        }
+    }
+    let tokens = quote!{
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct(stringify!(#name))
+                    .#(#debug_fields).*
+                    .finish()
+            }
+        }
+    };
+    tokens.into()
 }
