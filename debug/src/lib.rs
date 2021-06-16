d637962ced0a3a0c148ab008f3070d84bd3edc1d
wanghui <wh_linux@126.com>
2021-06-16 14:11:28 +0800

    debug 3/8: support debug attribute

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,7 +2,8 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{self, parse, DeriveInput};
 
-#[proc_macro_derive(CustomDebug)]
+
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = parse(input).unwrap();
     let name = &ast.ident;
@@ -13,6 +14,19 @@ pub fn derive(input: TokenStream) -> TokenStream {
         if let syn::Fields::Named(fields) = ds.fields {
             for field in fields.named.iter() {
                 let field_name = field.ident.clone().unwrap();
+                if let Some(attr) = field.attrs.clone().iter().next() {
+                    match parse_debug_attr_value(&attr) {
+                        Ok(v) => {
+                            if let Some(value) = v {
+                                debug_fields.push(quote!{
+                                    field(stringify!(#field_name), &format_args!(#value, &self.#field_name))
+                                });
+                                continue;
+                            }
+                        },
+                        Err(err) => return err.to_compile_error().into(),
+                    }
+                }
                 debug_fields.push(quote!{
                     field(stringify!(#field_name), &self.#field_name)
                 });
@@ -29,4 +43,29 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     };
     tokens.into()
+}
+fn parse_debug_attr_value(attr: &syn::Attribute) -> Result<Option<syn::LitStr>, syn::Error> {
+    if let Some(seg) = attr.path.segments.first() {
+        if seg.ident != "debug" {
+            return Ok(None);
+        }
+        let meta: syn::Meta = attr.parse_meta()?;
+        match meta {
+            syn::Meta::NameValue(values) => {
+                let arg_name = &values.path.segments.first().unwrap().ident;
+                if arg_name == "debug" {
+                    if let syn::Lit::Str(name) = values.lit {
+                        return Ok(Some(name));
+                    }
+                } else {
+                    let start = attr.path.segments.first().unwrap().ident.span();
+                    return Err(syn::Error::new(start, "expected `builder(each = \"...\")`".to_owned()));
+                }
+            },
+            _ => {
+                return Err(syn::Error::new(attr.bracket_token.span, "expected `debug = `\"...\"`".to_owned()));
+            },
+        }
+    }
+    return Ok(None);
 }
