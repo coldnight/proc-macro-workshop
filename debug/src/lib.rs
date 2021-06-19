3bbf58e3f9772a13572d1ecfb4c3384412a20485
wanghui <wh_linux@126.com>
2021-06-19 11:53:15 +0800

    refactor(debug): make 5 & 6 work together

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -16,7 +16,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
     if let syn::Data::Struct(ds) = ast.data {
         if let syn::Fields::Named(fields) = ds.fields {
             for field in fields.named.iter() {
-                let field_name = field.ident.clone().unwrap();
+                let field_ty = &field.ty;
+                let field_name = &field.ident;
                 if let Some(attr) = field.attrs.clone().iter().next() {
                     match parse_debug_attr_value(&attr) {
                         Ok(v) => {
@@ -33,18 +34,40 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 debug_fields.push(quote!{
                     field(stringify!(#field_name), &self.#field_name)
                 });
-            }
 
-            for p in ast.generics.params.iter() {
-                if let syn::GenericParam::Type(t) = p {
-                    let name = &t.ident;
-                    params.push(name);
-                    if is_param_wrapped_phantom(name, &fields) {
-                        wheres.push(quote!(std::marker::PhantomData<#name>: std::fmt::Debug))
-                    } else {
-                        wheres.push(quote!(#name: std::fmt::Debug))
+                let generics = &ast.generics;
+
+                if is_wrapped_generic_param(field_ty, &|ty| is_generic_param(ty, generics)) {
+                    if is_generic_param(field_ty, generics)  || is_debug_container(field_ty) {
+                        wheres.push(quote!(#field_ty: std::fmt::Debug));
+                    } else if let Some(tp) = unwrap_generics_param(field_ty, &|ty| is_generic_param(ty, generics)){
+                        let mut names = vec![];
+                        for seg in tp.path.segments.iter() {
+                            names.push(&seg.ident);
+                        }
+                        wheres.push(quote!(#(#names)::*: std::fmt::Debug));
                     }
                 }
+            }
+        }
+    }
+    for p in ast.generics.params.iter() {
+        if let syn::GenericParam::Type(t) = p {
+            let name = &t.ident;
+            let mut trait_bounds = vec![];
+            for b in t.bounds.iter() {
+                if let syn::TypeParamBound::Trait(tb) = b {
+                    let mut seg_idents = vec![];
+                    for seg in tb.path.segments.iter() {
+                        seg_idents.push(&seg.ident);
+                    }
+                    trait_bounds.push(quote!(#(#seg_idents)::*))
+                }
+            }
+            if trait_bounds.len() > 0 {
+                params.push(quote!(#name: #(#trait_bounds)+*));
+            } else {
+                params.push(quote!(#name));
             }
         }
     }
@@ -88,7 +111,7 @@ fn parse_debug_attr_value(attr: &syn::Attribute) -> Result<Option<syn::LitStr>, 
                     }
                 } else {
                     let start = attr.path.segments.first().unwrap().ident.span();
-                    return Err(syn::Error::new(start, "expected `builder(each = \"...\")`".to_owned()));
+                    return Err(syn::Error::new(start, "expected `debug = \"...\"`".to_owned()));
                 }
             },
             _ => {
@@ -99,36 +122,61 @@ fn parse_debug_attr_value(attr: &syn::Attribute) -> Result<Option<syn::LitStr>, 
     return Ok(None);
 }
 
+// returns true if generic param wrapped via others like PhantomData
+fn is_wrapped_generic_param(field_ty: &syn::Type, is_generic_param: &dyn Fn(&syn::Type) -> bool) -> bool {
+    if let Some(_) = unwrap_generics_param(field_ty, is_generic_param) {
+        return true;
+    }
+    false
+}
 
-fn is_param_wrapped_phantom(name: &syn::Ident, fields: &syn::FieldsNamed) -> bool {
-    let mut ret = false;
-    for field in fields.named.iter() {
-        let field_ty = &field.ty;
-        if let syn::Type::Path(ty_pth) = field_ty {
-            for seg in ty_pth.path.segments.iter() {
-                // type directly use
-                if &seg.ident == name {
-                    return false;
+
+fn is_generic_param(field_ty: &syn::Type, generics: &syn::Generics) -> bool {
+    if let syn::Type::Path(ty) = field_ty {
+        if ty.path.segments.len() == 0 {
+            return false;
+        }
+        let name = ty.path.segments.first().unwrap();
+
+        for p in generics.params.iter() {
+            if let syn::GenericParam::Type(t) = p {
+                if t.ident == name.ident {
+                    return true
                 }
-                if let syn::PathArguments::AngleBracketed(args) = &seg.arguments {
-                    for item in args.args.iter() {
-                        if let syn::GenericArgument::Type(ty) = item {
-                            if let syn::Type::Path(ty_path) = ty {
-                                if &(ty_path.path.segments.iter().next().unwrap().ident) == name {
-                                    // if container is PhantomData then T is wrapped via PhantomData,
-                                    // otherwise we return false immedialy.
-                                    if seg.ident == "PhantomData" {
-                                        ret = true;
-                                    } else {
-                                        return false;
-                                    }
-                                }
-                            }
-                        }
+            }
+        }
+    }
+    return false;
+}
+
+fn is_debug_container(ty: &syn::Type) -> bool {
+    if let syn::Type::Path(tp) = ty {
+        if tp.path.segments.len() == 0 {
+            return false;
+        }
+        return tp.path.segments.first().unwrap().ident == "PhantomData";
+    }
+    return false;
+}
+
+
+fn unwrap_generics_param<'a>(field_ty: &'a syn::Type, is_generic_param: &dyn Fn(&'a syn::Type) -> bool) -> Option<&'a syn::TypePath> {
+    if is_generic_param(field_ty) {
+        if let syn::Type::Path(ret) = field_ty {
+            return Some(ret);
+        }
+    }
+
+    if let syn::Type::Path(ty) = field_ty {
+        for seg in ty.path.segments.iter() {
+            if let syn::PathArguments::AngleBracketed(args) = &seg.arguments {
+                for item in args.args.iter() {
+                    if let syn::GenericArgument::Type(ty) = item {
+                        return unwrap_generics_param(ty, is_generic_param);
                     }
                 }
             }
         }
     }
-    return ret;
+    return None;
 }
